37a91e85ad7e12026df5be17494ffebeb46acbc8
MoyArt <moyart@gmail.com>
2018-05-03 17:31:23 -0500

    home.htm change to home.html, forecast.htm change to forecast.html

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 weatherApp.config(function($routeProvider){
     $routeProvider
         .when('/', {
-            templateUrl: 'pages/home.htm',
+            templateUrl: 'pages/home.html',
             controller: 'homeController'
         })
         .when('/forecast', {
-            templateUrl: 'pages/forecast.htm',
+            templateUrl: 'pages/forecast.html',
             controller: 'forecastController'
         })
 });
